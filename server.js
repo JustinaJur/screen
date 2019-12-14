4589d4da6e11a66431a71848ca0478faa035871e
Justina <44163331+JustinaJur@users.noreply.github.com>
2019-12-14 11:02:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-// var express = require("express");
-// var app = express();
-// app.use(express.static(__dirname + "/"));
-// app.listen(process.env.PORT || 8080);
-
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
